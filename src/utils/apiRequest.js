76ae0e16a5053668a830960b6f663da58a1cc584
DEV-A7MED <elahwal45@gmail.com>
2023-06-06 17:13:36 +0300

    update BE url

--- src/utils/apiRequest.js
+++ src/utils/apiRequest.js
@@ -2,6 +2,6 @@
 import axios from 'axios'
 
 const requset=axios.create({
-    baseURL:"https://blogs-app-back.vercel.app/api"
+    baseURL:"https://blogs-app-back-i1mobaxfo-dev-a7med.vercel.app/api"
 })
 export default requset
